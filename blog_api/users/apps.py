7a57a4ca243a74ca2b350b3eda4285c35951b772
GitDurrel Kamga <kamgadurrel@gmail.com>
2025-03-04 11:36:42 +0000

    first commit

--- blog_api/users/apps.py
+++ blog_api/users/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BlogApiConfig(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog_api'
+    name = 'users'
