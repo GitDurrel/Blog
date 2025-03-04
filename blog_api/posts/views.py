99cdf69cd940f70604e31c904d81d241366a992c
GitDurrel Kamga <kamgadurrel@gmail.com>
2025-03-04 14:17:38 +0000

    version 2.1

--- blog_api/posts/views.py
+++ blog_api/posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Post
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 
@@ -9,12 +11,30 @@ def posts_lists(request):
 
     posts = Post.objects.all()
     data = list(posts.values('id', 'titre', 'contenu', 'auteur', 'date_publication'))
-    
+
     return JsonResponse({'posts': data})
 
 
-def add_posts():
-    pass
+def add_posts(request):
+    if request.method == 'POST':
+        try:
+            # Charger les données envoyées dans le corps de la requête
+            data = json.loads(request.body)
+            
+            # Créer un nouveau post
+            post = Post.objects.create(
+                titre=data['titre'],
+                contenu=data['contenu'],
+                auteur=data['auteur']
+            )
+            
+            # Retourner une réponse JSON confirmant l'ajout
+            return JsonResponse({'message': 'Post créé avec succès', 'id': post.id}, status=201)
+        
+        except KeyError as e:
+            return JsonResponse({'error': f'Clé manquante: {str(e)}'}, status=400)
+    return JsonResponse({'error': 'Méthode non autorisée'}, status=405)
+
 
 
 def posts_details():
